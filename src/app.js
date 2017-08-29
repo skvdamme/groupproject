0e7a7d1cebe39bd21171b04414a8d68e04961629
yeropharaoh <yeroschutte@gmail.com>
2017-08-29 11:49:39 +0200

    deleted files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-const bcrypt = require('bcrypt');
 const Sequelize = require('sequelize');
 const express = require('express');
-let bodyParser = require('body-parser');
-let session = require('express-session');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
-let sequelize = new Sequelize(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:5000/group_proj`);
+const sequelize = new Sequelize('blogapplication', 'samantha_kaylee', null, {
+	host: 'localhost',
+	dialect: 'postgres'
+});
 
 const app = express();
 
@@ -16,14 +18,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(session({
 	secret: "whatever",
 	saveUninitialized: true,
-	resave: true
+	resave: false
 }));
 
-const User = sequelize.define('users',{
-	username:{
-		type: Sequelize.STRING,
-		unique: true
-	},
+const User = sequelize.define('user',{
 	email: {
 		type: Sequelize.STRING,
 		unique: true
@@ -35,21 +33,6 @@ const User = sequelize.define('users',{
 		timestamps:false
 	});
 
-
-const PackingList  = sequelize.define('packing_list',{
-	items:{
-		type: Sequelize.ARRAY(Sequelize.TEXT)
-	},
-	}, {
-		timestamps:false
-	});
-
-
-//Establishing relationships between models
-User.hasOne(PackingList);
-PackingList.belongsTo(User);
-
-
 app.get('/', function(req,res){
 	res.render('index', {
 		message: req.query.message,
@@ -57,173 +40,87 @@ app.get('/', function(req,res){
 	});
 });
 
-app.post('/register', (req,res)=>{
-	var password = req.body.rgpassword
-	  bcrypt.hash(password, 8, (err, hash) => { //created hash for password security
-	      if (err) throw err;
-	      User.create({
-	          username: req.body.rgusername,
-	          email: req.body.rgemail,
-	          password: hash
-	      })
-	          .then((user) => {
-	              console.log("User create promise returned success!")
-	              req.session.user = user;
-	              res.redirect('/categories');
-	              console.log('do i make it here?')
-	          })
-	          .catch((error) => {
-	              console.error(error)
-	          })
-	});
-
+app.get('/register', function(req,res){
+	console.log(req.session)
+	res.render('register')
 });
+
+app.post('/register', (req,res)=>{
+	User.create({
+		email: req.body.email,
+		password: req.body.password
+	})
+	.then((user) => {
+		req.session.user = user;
+		res.redirect('/profile');
+	}).catch((error) =>{
+		console.log(error)
+	});
+}); 
 
 app.get('/login', function(req,res){
-    res.render('login', {user:req.session.user})
+	res.render('login', {user:req.session.user})
 });
 
-app.post('/login', function (request, response) {
-  if(request.body.email.length === 0) {
-    response.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
-    return;
-  }
+app.post('/login', function (req, res) {
+	if(req.body.email.lentgh === 0) {
+		res.redirect('/?message=' + encodeURIComponent("please fill out your email adress"));
+	return;
+	}
 
+	if(req.body.password.length === 0) {
+		response.redirect('/?message=' + encodeURIComponent("please fill out your password"));
+		return;
+	}
+var email = req.body.email
+var password = req.body.password
 
-
- if (request.body.password.length === 0) {
-
-    response.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
-    return;
-  }
-  var email = request.body.email
-  var password = request.body.password
-
-
-  console.log(password);
-
-  User.findOne({
-
-          where: {
-              email: email
-          }
-      })
-      .then((user) => {
-
-      	if (!user){
-      		response.redirect('/?message=' + encodeURIComponent("User doesn't exist."));
-      	}
-
-        else {
-        bcrypt.compare(password, user.password, (err, res) => { //validates password
-         // console.log('Entered hashed password'+ password)
-        // console.log('Database password'+user.password);
-         if (res) {
-
-         	 request.session.user = user;
-             response.redirect('/categories');
-     		}
-     	 else {
-     	 	response.redirect('/?message=' + encodeURIComponent("Incorrect password."));
-     	 }	
-     	})
-   		}
-	});
-});
-    
-app.get('/categories', (req,res)=>{
-	var user = req.session.user;
-	if (user === undefined) {
-		res.redirect('/?message=' + encodeURIComponent("please log in to view your profile"))
-	} else {
-	res.render('categories', {
-		user: user
-	});
-   }
-});
-
-app.get('/sunlist', (req,res)=>{
-	var user = req.session.user;
-	if (user === undefined) {
-		res.redirect('/?message=' + encodeURIComponent("please log in to view your profile"))
-	} else {
-	res.render('sunlist', {
-		user: user
-	});
-
-   }
-});
-
-app.post('/sunlist', (req,res)=>{
-	var forminfo = req.body
-	var user = req.session.user;
-
-	var emptyArray1 = []
-	var emptyArray2 = []
-
-for (var X in forminfo) {
-   if ( ! forminfo.hasOwnProperty(X)) {
-      continue;
-   }
-  emptyArray1.push(X);
-  emptyArray2.push(forminfo[X]);
-}
-
-console.log(emptyArray1);
-console.log(emptyArray2);
-console.log(forminfo)
-
-PackingList.create({
-          items: emptyArray2,
-          userId: user.id
-      })
-.then(packinglist =>{
-   var packinglist = packinglist;
-
-   res.render('/profile', {packinglist: packinglist})
-
+User.findOne({
+	where: {
+		email: email
+	}
 })
-
-	res.redirect('sunlist');
+.then(function(user) {
+	console.log('user email '+ user.email)
+	console.log('user password ' + user.password)
+	if (user != null && password === user.password) {
+		req.session.user = user;
+		res.redirect('/profile');
+	} else {
+		res.redirect('/?message=' + encodeURIComponent("invalid email or password"));
+	}
+  })
+.catch(function (error) {
+	console.error(error)
+	res.redirect('/?message=' + encodeURIComponent("invalid email or password"));
+	});
 });
 
 app.get('/profile', (req,res)=>{
-    var user = req.session.user;
-    if (user) {
-    User.findOne({
-    	where: {
-    		id: user.id
-    	}
-    })
-     .then((userfound)=>{
-     	PackingList.findOne({
-     		where: {
-     			userId: user.id
-     		}
-     	})
-     	.then(packinglist=>{
-     		res.render('profile', {user: userfound, packinglist: packinglist})
-     	})
-     
-    })
-}  else {
-  	res.redirect('/?message=' + encodeURIComponent("Please login to view your profile."));
-  }
+	var user = req.session.user;
+	if (user === undefined) {
+		res.redirect('/?message=' + encodeURIComponent("please log in to view your profile"))
+	} else {
+	res.render('profile', {
+		user: user
+	});
+   }
 });
-
-        
 
 app.get('/logout', (req,res)=>{
-    req.session.destroy((error) =>{
-        if(error){
-            throw error;
-        }
-        res.redirect('/?message=' + encodeURIComponent("succesfully logged out"));
-    });
+	req.session.destroy((error) =>{
+		if(error){
+			throw error;
+		}
+		res.redirect('/?message=' + encodeURIComponent("succesfully logged out"));
+	});
 });
 
-sequelize.sync({force:false});
+
+sequelize.sync();
 
 app.listen(3000, function(){
-    console.log('Hey is this thing on?!')
-})
+	console.log('Hey is this thing on?!')
+});
+
+
